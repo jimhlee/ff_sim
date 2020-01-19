54ccce5edc4fe790518df103b5dacee9e48371aa
James Lee <jimhlee23@gmail.com>
2020-01-18 20:27:44 -0500

    Data collection complete

--- ff_simulator.py
+++ ff_simulator.py
@@ -1,5 +1,6 @@
 # two functions, per and const, to handle the data
 import pandas as pd
+from rule_overrides import RULE_OVERRIDES
 from parse_player import parser
 
 RULES_DIRECTORY = '/Users/jimhlee23/learnpython/fantasy_football/raw_rules/'
@@ -18,32 +19,19 @@ FUNC_REF = {
 }
 
 def score_event(df, rule, val):
-    score_df = df[(df['category'] == rule) & (df['min'] <= val) & (df['max'] >=  val)]
+    score_df = df[(df['fixed_rule'] == rule) & (df['min'] <= val) & (df['max'] >=  val)]
     score_func = score_df['ref_func'].values[0]
     score_arg = score_df['ref_args'].values[0]
     return FUNC_REF[score_func](val, score_arg)
 
-'''
-def parse_position_rules(position):
-     raw_rules = pd.read_csv ...
-     #systematic stuff
-     for rule in raw_rules:
-         do_systematic_things
-     for rule, override_rule in RULE_OVERRIDES[position].items():
-         replace rule with override_rule
-     return final_rules
-'''
-
 # This will reform the irregularities in the rules and return them
 def get_parsed_rules(position):
-    # finished_rules = []
-    raw_rules = pd.read_csv(RULES_DIRECTORY + f'{position}.csv')
-    # for rule in raw_rules:
-    #     # fix that shit
-    # for rule, override_rule in RULE_OVERRIDES[position].items():
-    #     # replace rule with override_rule
-    return raw_rules
-    # correct the raw rules to match
+    rules = pd.read_csv(RULES_DIRECTORY + f'{position}.csv')
+    # I'm an idiot, wrote the dict backwards so I had to flip it
+    fixed_rule_overrides = {v: k for k, v in RULE_OVERRIDES[position].items()}
+    rules['fixed_rule'] = rules['category'].apply(lambda x: fixed_rule_overrides.get(x, x))
+    # rules['fixed_rule'] = rules['category'].apply(lambda x: fixed_rule_overrides[position].get(x, x))
+    return rules
 
 # This will reorganize the positions after parsing into a more easily usable form
 def score_players_by_position(players, rules):
@@ -63,18 +51,6 @@ def score_players_by_position(players, rules):
         players_dict[player_name] = weeks_dict
     return players_dict, failed_rules
 
-'''
-def thing_doer():
-      for position in positions:
-            players = all_players[position]
-            rules = get_rules_for_posn(position)
-            do_thing_to_position(players, rules)
-
-def do_thing_to_position(players, rules):
-     # i would have the big loop here
-     return
-'''
-
 # This is the main function, other functions will feed into it
 def score_all_players(all_players):
     output_dict = {}
@@ -86,16 +62,6 @@ def score_all_players(all_players):
         player_results, failed_rules = score_players_by_position(players, rules)
         output_dict[position] = {'player_results': player_results, 'baddies': failed_rules}
     return output_dict
-
-    # positions_dict = {}
-    # for position in positions:
-    #     rules = get_parsed_rules(position)
-
-    # is_fail = False
-    # if is_fail == True:
-    #     # compile a list
-    #     pass
-    # return positions_dict, failed
 
 if __name__ == '__main__':
     all_players = parser()
---
+++ rule_overrides.py
@@ -0,0 +1,55 @@
+RULE_OVERRIDES = {
+    'QB': {
+        'Interceptions': 'Pass Interceptions Thrown', 
+        'Rushes': 'Rush Attempts', 
+    },
+    'ST': {
+        'yd Blocked Punt TD': 'Length of Blocked Punt TD',
+        'yd Field Goal': 'Length of Field Goal Made',
+        'yd Field Goal Miss': 'Length of Missed Field Goal',
+        'yd Kickoff Return TD': 'Length of Kickoff Return TD',
+        'yd Punt Return TD': 'Length of Punt Return TD',
+    },
+    'WR': {
+        'Interceptions': 'Pass Interceptions Thrown', 
+        'Rushes': 'Rush Attempts',
+    },
+    'TE': {
+        'Interceptions': 'Pass Interceptions Thrown', 
+        'Rushes': 'Rush Attempts',
+    },
+    'S': {
+        'Interceptions': 'Interceptions Caught', 
+        'Rushes': 'Rush Attempts',
+    },
+    'RB': {
+        'Interceptions': 'Pass Interceptions Thrown', 
+        'Rushes': 'Rush Attempts',
+    },
+    'Off': {
+        'Completions': 'Pass Completions',
+        'Incomplete Passes': 'Pass Incompletions',
+        'Interceptions': 'Pass Interceptions Thrown',
+        'Sack Yards Lost': 'QB Sacked Yardage',
+        'yd Passing TD': 'Length of Passing TD',
+        'yd Rushing TD': 'Length of Rushing TD',
+    },
+    'LB': {
+        'Interceptions': 'Pass Interceptions Thrown',  
+    },
+    'Def': {
+        'Interceptions': 'Interceptions Caught', 
+        'yd Defensive TD': 'Length of Defensive TD'
+    },
+    'DE': {
+        'Interceptions': 'Interceptions Caught',
+    },
+    'CB': {
+        'Interceptions': 'Interceptions Caught',
+        'Rushes': 'Rush Attempts', 
+    },
+    'DT': {
+        'Interceptions': 'Interceptions Caught',
+        'Rushes': 'Rush Attempts', 
+    },
+}
